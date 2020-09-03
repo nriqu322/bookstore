e4bfc3f4dc97dfa42f48944c28f708bbc38e95ad
Luis Saavedra <nriqu322@gmail.com>
2020-09-03 17:00:29 -0600

    add book component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const BooksList = ({ books }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map(book => {
-          <div key={book.id} book={book} />;
-        })}
-      </tbody>
-    </table>
-  );
-};
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map(book => (
+        <div key={book.id} book={book} />
+      ))}
+    </tbody>
+  </table>
+);
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf.isRequired,
