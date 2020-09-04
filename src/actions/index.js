c23606746f5ba058990b0327b6a41d2303fa1095
Luis Saavedra <nriqu322@gmail.com>
2020-09-03 18:35:09 -0600

    fix eslint errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,16 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const createBook = book => {
-  return {
-    type: CREATE_BOOK,
-    book,
-  };
-};
+const createBook = book => ({
+  type: CREATE_BOOK,
+  book,
+});
 
-const removeBook = book => {
-  return {
-    type: REMOVE_BOOK,
-    id: book.id,
-  };
-};
+const removeBook = book => ({
+  type: REMOVE_BOOK,
+  id: book.id,
+});
 
-export { createBook, removeBook, CREATE_BOOK, REMOVE_BOOK };
+export {
+  createBook, removeBook, CREATE_BOOK, REMOVE_BOOK,
+};
