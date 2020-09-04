e2593cfb060ed9e40ba798cd87ed289d3e0555f1
Luis Saavedra <nriqu322@gmail.com>
2020-09-04 11:38:51 -0600

    BooksForm converted to a stateful component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import { createBook } from '../actions/index'
 
-const BooksForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <form id="book-form">
-      <input name="bookTitle" component="input" type="text" placeholder="add book title" />
-      <select name="category" value="">
-        {categories.map(category => (
-          <option key={category} value={category}>{category}</option>
-        ))}
-      </select>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { title, category } = this.state;
+    const { createBook } = this.props;
+
+    const book = {
+      id: Math.floor(Math.random() * 5000),
+      title,
+      category
+    }
+    this.setState({ title: ''});
+    createBook(book);
+    }
+  }
+
+  render() {
+    const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    const { title, category } = this.state;
+
+    return (
+      <form id="book-form" onSubmit={this.handleSubmit}>
+        <input name="title" value={title} type="text" placeholder="add book title" onChange={this.handleChange} />
+        <select name="category" value={category} onChange={this.handleChange} >
+          {categories.map(category => (
+            <option key={category} value={category} >{category}</option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  createBook: book => {
+    dispatch(createBook(book))
+  }
+})
 
 export default BooksForm;
