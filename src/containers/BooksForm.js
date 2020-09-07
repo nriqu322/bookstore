3a64898158d5b8106f3bd5dc00a5f6e5d56c9b16
Luis Saavedra <nriqu322@gmail.com>
2020-09-06 18:35:15 -0600

    remove unnecessary value on select

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -39,12 +39,12 @@ class BooksForm extends React.Component {
 
   render() {
     const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-    const { title, category } = this.state;
+    const { title } = this.state;
 
     return (
       <form id="book-form" onSubmit={this.handleSubmit}>
         <input name="title" value={title} type="text" placeholder="add book title" onChange={this.handleChange} />
-        <select name="category" value={category} onChange={this.handleChange}>
+        <select name="category" onChange={this.handleChange}>
           {categories.map(category => (
             <option key={category} value={category}>{category}</option>
           ))}
