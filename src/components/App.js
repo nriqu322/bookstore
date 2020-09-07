95bd983e00c5e8173e69df7e2f5f4464352a0d7a
Luis Saavedra <nriqu322@gmail.com>
2020-09-07 11:30:42 -0600

    add fontawesome and header titles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BookList from '../containers/BooksList';
 import BookForm from '../containers/BooksForm';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="left-header">
+          <div className="bookstore-title">Bookstore CMS</div>
+          <ul>
+            <li>BOOKS</li>
+            <li>CATEGORIES</li>
+          </ul>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUser} />
+        </div>
+      </header>
       <BookList />
       <BookForm />
     </div>
